79d96a540bd7222395679a78b8a8d08f3503613f
Steven McLeod <steven@mcleodnet.com>
2020-02-13 20:59:37 -0800

    Added video compiler

---
+++ compile_video.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+VLIB="vlib.exe"
+VMAP="vmap.exe"
+VCOM="vcom.exe"
+VLOG="vlog.exe"
+VSIM="vsim.exe"
+
+set -e
+
+rm -rf work
+
+pushd hdl/video
+$VLIB ../../work
+$VMAP work ../../work
+
+# Common Components
+$VLOG ../plainrom.sv
+$VLOG ../plainram.sv
+
+$VLOG paletter.sv
+$VLOG tilegen.sv
+$VLOG dkong_video.sv
+$VLOG video_tb.sv +define+TESTFILE='"dkong.game"'
+#$VLOG video_tb.sv +define+TESTFILE='"dkong.title"'
+
+# Run Sim
+popd
+./testers/scrbuf/scrbuf.exe &
+$VSIM video_tb &
